18dca5b00cc448868f06292eb52ec21463803467
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-09-21 22:54:17 +0200

    add page PageNoteFound

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import StartingPage from "./components/StartingPage";
 import PomodoroTechnique from "./components/PomodoroTechnique";
 import AppLayout from "./components/AppLayout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<StartingPage />} />
         <Route path="appLayout" element={<AppLayout />} />
         <Route path="pomodoroTechnique" element={<PomodoroTechnique />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+function PageNotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-12">
+      <p className="text-6xl">Ooops...</p>
+      <p className="text-8xl">Page not found</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
